9db1a49a1f63de42ff0ab1227169c24a60f4eb47
Monil Jain <jainmonil2101@gmail.com>
2021-12-03 15:32:30 +0530

    Muskan Boutique

--- mb/settings.py
+++ mb/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = (
-    ('django.contrib.auth.backends.ModelBackend'),
-)
+# AUTHENTICATION_BACKENDS = (
+#     ('django.contrib.auth.backends.ModelBackend'),
+# )
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
