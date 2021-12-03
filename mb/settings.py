88b41e23411b7a817f221c3164b175021a6f809f
Monil Jain <jainmonil2101@gmail.com>
2021-12-03 14:45:39 +0530

    Muskan Boutique

--- mb/settings.py
+++ mb/settings.py
@@ -82,8 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'root',
-        'PASSWORD': 'mb2101',
+        
     }
 }
 
